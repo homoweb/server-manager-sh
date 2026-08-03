bded409ad4ed51c2b73a56c4e9574ec8713209c1
Hossein Mohebi <h.mohebi996@gmail.com>
2026-08-03 18:17:25 +0330

    Update server_manager.sh

--- server_manager.sh
+++ server_manager.sh
@@ -207,17 +207,34 @@ EOF
     cat <<EOF > "$VHOST_CONF"
 server {
     listen 80;
-    server_name $DOMAIN;
-    root /home/$USERNAME/public;
-    index index.php index.html index.htm;
-    
+    server_name $DOMAIN www.$DOMAIN;
+    root /home/$USERNAME/$DOMAIN/public;
+
+    add_header X-Frame-Options "SAMEORIGIN";
+    add_header X-XSS-Protection "1; mode=block";
+    add_header X-Content-Type-Options "nosniff";
+
+    index index.html index.htm index.php;
+
+    charset utf-8;
+
     location / {
         try_files \$uri \$uri/ /index.php?\$query_string;
     }
-    
+
+    location = /favicon.ico { access_log off; log_not_found off; }
+    location = /robots.txt  { access_log off; log_not_found off; }
+
+    error_page 404 /index.php;
+
     location ~ \.php$ {
-        include snippets/fastcgi-php.conf;
-        fastcgi_pass unix:/run/php/php8.4-fpm-$USERNAME.sock;
+        fastcgi_pass unix:/var/run/php/php8.4-fpm.sock;
+        fastcgi_param SCRIPT_FILENAME \$realpath_root\$fastcgi_script_name;
+        include fastcgi_params;
+    }
+
+    location ~ /\.(?!well-known).* {
+        deny all;
     }
 }
 EOF
